079e7d949a876d4e098b62bda8f8d551606bda4b
sadrisso <sadriisso304@gmail.com>
2025-03-20 14:09:06 +0600

    boards api added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oq68b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,10 +27,25 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const boardsCollection = client.db("timeDB").collection("boards")
 
+    app.get("/boards", async (req, res) => {
+      const result = await boardsCollection.find().toArray()
+      res.send(result)
+    })
 
+    app.post("/boards", async (req, res) => {
+      const data = req.body;
+      const result = await boardsCollection.insertOne(data)
+      res.send(result)
+    })
 
-
+    app.delete("/boards/:id", async (req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await boardsCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
